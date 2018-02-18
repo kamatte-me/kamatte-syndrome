4b8f589de6e716652b8009c41a9423418c4df1b0
Shinnosuke Uwai <uydasan@gmail.com>
2018-02-19 00:04:46 +0900

    add: apply FLOCCS add: avatar image

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,10 @@
     <section class="hero is-fullheight is-primary">
       <div class="hero-body">
         <div class="container is-fluid has-text-centered">
-          <h1 class="eyecatch">
-            kamatte<img src="/logo.png" width="60px" height="60px" alt=""/>syndrome
+          <h1 class="p-index_eyecatch">
+            kamatte
+            <img class="p-index_eyecatch-logo" src="/logo.png" width="60px" height="60px"/>
+            syndrome
           </h1>
         </div>
       </div>
@@ -12,7 +14,8 @@
     <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container is-fluid has-text-centered has-text-green">
-          <span class="eyecatch">
+          <span class="p-index_eyecatch">
+            <img class="p-index_eyecatch-avatar" src="/avatar.png" width="136" height="136"/>
             plz kamatte me!!!
           </span>
         </div>
