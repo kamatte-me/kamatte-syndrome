0119264d8736f61a0066ec3bc427c3201659e504
Shinnosuke Uwai <uydasan@gmail.com>
2018-02-18 20:03:09 +0900

    mod: favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'plz kamatte me!!!' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
     ],
   },
   /*
