cd836e9689b9f2d13dd1e898f9fc4e3020abba4d
Shinnosuke Uwai <uydasan@gmail.com>
2018-02-18 19:10:17 +0900

    navbar fixed top

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,8 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-fixed-top"
+       role="navigation"
+       aria-label="main navigation"
+  >
     <div class="navbar-brand">
       <a class="navbar-item" href="/">
         <img src="../assets/logo.png" alt="kamatte syndrome"/>
