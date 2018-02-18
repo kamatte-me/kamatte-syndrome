cfe9d5555ded5a923e406e3a99061f6b088aa779
Shinnosuke Uwai <uydasan@gmail.com>
2018-02-18 21:09:03 +0900

    add: index page rough design mod: primary color mod: navbar color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar is-fixed-top"
+  <nav class="navbar is-primary is-fixed-top"
        role="navigation"
        aria-label="main navigation"
   >
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,24 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        kamatte-syndrome
-      </h1>
-      <h2 class="subtitle">
-        Next kamatte syndrome
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div>
+    <section class="hero is-fullheight is-primary">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title">
+            kamatte syndrome
+          </h1>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+    <section class="hero is-fullheight">
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-primary">
+            plz kamatte me!!!
+          </h1>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -31,34 +30,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
