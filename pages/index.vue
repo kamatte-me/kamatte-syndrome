aede662510569a051d61b556d6bdd85b93dcf931
Shinnosuke Uwai <uydasan@gmail.com>
2018-02-19 00:25:48 +0900

    fix: broken layout on mobile

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <section class="hero is-fullheight is-primary">
       <div class="hero-body">
         <div class="container is-fluid has-text-centered">
-          <h1 class="p-index_eyecatch">
+          <h1 class="title p-index_eyecatch">
             kamatte
             <img class="p-index_eyecatch-logo" src="/logo.png" width="60px" height="60px"/>
             syndrome
@@ -13,9 +13,9 @@
     </section>
     <section class="hero is-fullheight">
       <div class="hero-body">
-        <div class="container is-fluid has-text-centered has-text-green">
-          <span class="p-index_eyecatch">
-            <img class="p-index_eyecatch-avatar" src="/avatar.png" width="136" height="136"/>
+        <div class="container is-fluid has-text-centered">
+          <span class="title has-text-green p-index_eyecatch">
+            <img class="p-index_eyecatch-avatar" src="/avatar.png" width="130" height="130"/>
             plz kamatte me!!!
           </span>
         </div>
