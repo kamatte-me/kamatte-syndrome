33859e91fa42f2c39d720ca5624a3d02bf2cc676
Shinnosuke Uwai <uydasan@gmail.com>
2018-02-25 02:06:29 +0900

    add: 年情報を付与

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -3,30 +3,39 @@
     <page-header/>
     <section class="section">
       <div class="container">
-        <div class="columns" v-for="(work, index) in works" :key="index">
-          <div class="column is-one-third has-text-centered">
-            <img :src="work.imagePath" width="150" height="150"/>
-          </div>
-          <div class="column">
-            <div class="content">
-              <div class="block has-text-centered-mobile">
-                <h2 class="title">
-                  <a v-if="work.url !== undefined" :href="work.url" target="_blank">
-                    {{ work.title }}
-                  </a>
-                  <template v-else>
-                    {{ work.title }}
-                  </template>
-                </h2>
-                <p class="subtitle is-6 has-text-grey-light">{{ work.charge }}</p>
-              </div>
-              <p v-html="work.description"></p>
+        <div class="p-portfolio_worksWrap"
+             v-for="portfolio in sortedPortfolio" :key="portfolio.year"
+        >
+          <h1 class="title has-text-centered p-portfolio_year">
+            {{ portfolio.year }}
+          </h1>
+          <div class="columns p-portfolio_worksWrap_workWrap"
+               v-for="(work, index) in portfolio.works" :key="index"
+          >
+            <div class="column is-one-third has-text-centered">
+              <img :src="work.imagePath" width="150" height="150"/>
             </div>
-            <aside class="tags">
-              <div class="tag" v-for="tag in work.tags" :key="tag">
-                {{ tag }}
+            <div class="column">
+              <div class="content">
+                <div class="block has-text-centered-mobile">
+                  <h2 class="title">
+                    <a v-if="work.url !== undefined" :href="work.url" target="_blank">
+                      {{ work.title }}
+                    </a>
+                    <template v-else>
+                      {{ work.title }}
+                    </template>
+                  </h2>
+                  <p class="subtitle is-6 has-text-grey-light">{{ work.charge }}</p>
+                </div>
+                <p v-html="work.description"></p>
               </div>
-            </aside>
+              <aside class="tags">
+                <div class="tag" v-for="tag in work.tags" :key="tag">
+                  {{ tag }}
+                </div>
+              </aside>
+            </div>
           </div>
         </div>
       </div>
@@ -35,6 +44,7 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import PageHeader from '../components/PageHeader.vue';
 
 export default {
@@ -45,44 +55,59 @@ export default {
   data() {
     return {
       pageTitle: 'Portfolio',
-      works: [
+      portfolio: [
         {
-          title: 'kamatte syndrome',
-          imagePath: '/avatar.png',
-          charge: 'ソロプロジェクト',
-          description: 'いま、あなたといる、ここ。',
-          tags: [
-            'JavaScript',
-            'Vue',
-            'Nuxt.js',
-            'HTML',
-            'SCSS',
-            'Bulma',
-            'GCP',
-            'Firebase',
+          year: 2018,
+          works: [
+            {
+              title: 'kamatte syndrome',
+              imagePath: '/avatar.png',
+              charge: 'ソロプロジェクト',
+              description: 'いま、あなたといる、ここ。',
+              tags: [
+                'JavaScript',
+                'Vue',
+                'Nuxt.js',
+                'HTML',
+                'SCSS',
+                'Bulma',
+                'GCP',
+                'Firebase',
+              ],
+            },
           ],
         },
         {
-          title: '集団活動',
-          url: 'https://syudan.me',
-          imagePath: '/avatar.png',
-          charge: 'ソロプロジェクト',
-          description: 'バンド「<a href="https://www.syudan.com">集団行動</a>」の非公式ファンサイトです。',
-          tags: [
-            'Ruby',
-            'Ruby on Rails',
-            'JavaScript',
-            'React',
-            'HTML',
-            'CSS',
-            'Semantic UI',
-            'MariaDB',
-            'Nginx',
-            'Ubuntu',
+          year: 2017,
+          works: [
+            {
+              title: '集団活動',
+              url: 'https://syudan.me',
+              imagePath: '/avatar.png',
+              charge: 'ソロプロジェクト',
+              description: 'バンド「<a href="https://www.syudan.com">集団行動</a>」の非公式ファンサイトです。',
+              tags: [
+                'Ruby',
+                'Ruby on Rails',
+                'JavaScript',
+                'React',
+                'HTML',
+                'CSS',
+                'Semantic UI',
+                'MariaDB',
+                'Nginx',
+                'Ubuntu',
+              ],
+            },
           ],
         },
       ],
     };
+  },
+  computed: {
+    sortedPortfolio() {
+      return _.sortBy(this.portfolio, 'year').reverse();
+    },
   },
   created() {
     this.$store.commit('updatePageTitle', this.pageTitle);
