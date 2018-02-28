30b84366ecc7c10e7bdfeccc361609304b512bae
Shinnosuke Uwai <uydasan@gmail.com>
2018-03-01 01:32:13 +0900

    add: トップページにBiographyへのリンク設置

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,12 @@
     <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container is-fluid has-text-centered">
-          <p class="title is-1 has-text-green p-index_speech">
-            <img class="p-index_eyecatch-avatar" src="/avatar.png" width="130" height="130"/>
-            plz kamatte me!!!
-          </p>
+          <nuxt-link to="/biography">
+            <p class="title is-1 has-text-green p-index_speech">
+              <img class="p-index_eyecatch-avatar" src="/avatar.png" width="130" height="130"/>
+              plz kamatte me!!!
+            </p>
+          </nuxt-link>
         </div>
       </div>
     </section>
