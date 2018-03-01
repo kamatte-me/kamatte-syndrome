8a6e0fe872eb3cac6e9e92f5e62bb3fb8e0e68bd
Shinnosuke Uwai <uydasan@gmail.com>
2018-03-02 00:09:44 +0900

    fix: サムネイル取得中でも404表示されていたバグを修正

--- components/YoutubeEmbed.vue
+++ components/YoutubeEmbed.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="c-youtubeEmbed_videoWrap">
-    <div class="c-youtubeEmbed_videoWrap--video" v-if="thumbnailUrl !== null">
-      <button @click.once="play"
-              :style="{backgroundImage: `url(${thumbnailUrl})`}"
-              v-if="!isPlay">
-        <span></span>
-      </button>
-      <iframe :src="videoEmbedUrl"
-              allow="autoplay; encrypted-media"
-              frameborder="0" allowfullscreen
-              v-else>
-      </iframe>
+    <div class="c-youtubeEmbed_videoWrap--video">
+      <div class="c-youtubeEmbed_videoWrap--video-none" v-if="thumbnailUrl === null"></div>
+      <template v-else>
+        <button @click.once="play"
+                :style="{backgroundImage: `url(${thumbnailUrl})`}"
+                v-if="!isPlay">
+          <span></span>
+        </button>
+        <iframe :src="videoEmbedUrl"
+                allow="autoplay; encrypted-media"
+                frameborder="0" allowfullscreen
+                v-else>
+        </iframe>
+      </template>
     </div>
-    <div class="c-youtubeEmbed_videoWrap--novideo" v-else></div>
   </div>
 </template>
 
@@ -26,7 +28,7 @@ export default {
   data() {
     return {
       isPlay: false,
-      thumbnailUrl: null,
+      thumbnailUrl: '',
     };
   },
   computed: {
@@ -58,6 +60,11 @@ export default {
         }
         return true;
       });
+      if (this.thumbnailUrl === '') {
+        this.thumbnailUrl = null;
+      }
+    }).catch(() => {
+      this.thumbnailUrl = null;
     });
   },
 };
