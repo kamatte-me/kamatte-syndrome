f48eef73d279fb84c71ee3e82296b692cab9beea
Shinnosuke Uwai <uydasan@gmail.com>
2018-03-03 06:55:06 +0900

    add: ラフデザイン

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="hero is-fullheight is-green">
+    <div class="hero-body">
+      <div class="container has-text-centered l-error_status">
+        <h1 class="title is-1 has-text-primary l-error_status--code">
+          {{ error.statusCode }}
+        </h1>
+        <h2 class="subtitle is-4 has-text-primary l-error_status--reason">
+          <template v-if="error.statusCode === 404">
+            This page exists in the future!!!
+          </template>
+          <template v-else>su ma n</template>
+        </h2>
+        <nuxt-link to="/" class="button is-outlined is-primary is-medium">
+          TOP
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  props: ['error'],
+  head() {
+    return {
+      htmlAttrs: {
+        class: '',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
