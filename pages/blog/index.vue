08f7ffb80232a540487346184b41f3fed6006d3f
Shinnosuke Uwai <uydasan@gmail.com>
2018-03-04 18:39:50 +0900

    add: indexページの準備

---
+++ pages/blog/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <page-header/>
+    <section class="section">
+      <ul>
+        <li>test</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+import PageHeader from '../../components/PageHeader.vue';
+
+export default {
+  name: 'index',
+  components: {
+    PageHeader,
+  },
+  data() {
+    return {
+      pageTitle: 'Blog',
+    };
+  },
+  created() {
+    this.$store.commit('updatePageTitle', this.pageTitle);
+  },
+};
+</script>
