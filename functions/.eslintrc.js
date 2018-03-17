df81055c096f6c4d9ac3d151e55e17dc15461e34
Shinnosuke Uwai <uydasan@gmail.com>
2018-03-17 19:37:38 +0900

    mod: ESlintの設定

---
+++ functions/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    'airbnb',
+  ],
+  // add your custom rules here
+  rules: {},
+};
