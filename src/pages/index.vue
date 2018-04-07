b8441fdd2614d6e9ebbb717b37bbcd5889a124a4
Shinnosuke Uwai <uydasan@gmail.com>
2018-04-08 03:26:59 +0900

    add: imgにalt属性を設定

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,7 +5,7 @@
         <div class="container is-fluid has-text-centered">
           <h1 class="title p-index_eyecatch">
             kamatte
-            <img class="p-index_eyecatch-logo" src="/logo.png" width="60px" height="60px"/>
+            <img class="p-index_eyecatch-logo" src="/logo.png" width="60px" height="60px" alt=""/>
             syndrome
           </h1>
         </div>
@@ -16,7 +16,11 @@
         <div class="container is-fluid has-text-centered">
           <nuxt-link to="/biography">
             <p class="title is-1 has-text-green p-index_speech">
-              <img class="p-index_eyecatch-avatar" src="/avatar.png" width="130" height="130"/>
+              <img class="p-index_eyecatch-avatar"
+                   src="/avatar.png"
+                   width="130"
+                   height="130"
+                   alt="kamatte"/>
               plz kamatte me!!!
             </p>
           </nuxt-link>
