2a2162e6c88beee3842e5b55ee457e0cd66e0364
kamatte <uydasan@gmail.com>
2018-09-12 00:05:05 +0900

    fix: エラーページのレイアウト崩れを修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,32 +1,24 @@
 <template>
-  <div>
-    <header>
-      <navbar/>
-    </header>
-    <main>
-      <section class="hero is-fullheight is-green">
-        <div class="hero-body">
-          <div class="container has-text-centered l-error_status">
-            <h1 class="title is-1 has-text-primary l-error_status--statusCode">
-              {{ error.statusCode }}
-            </h1>
-            <h2 class="subtitle is-4 has-text-primary l-error_status--message">
-              <template v-if="message[error.statusCode] === undefined">
-                suman
-              </template>
-              <template v-else>
-                {{ message[error.statusCode] }}
-              </template>
-            </h2>
-            <nuxt-link to="/" class="button is-rounded is-outlined is-primary is-medium">
-              TOP
-            </nuxt-link>
-          </div>
-        </div>
-      </section>
-    </main>
-    <site-footer/>
-  </div>
+  <section class="hero is-fullheight is-green">
+    <div class="hero-body">
+      <div class="container has-text-centered l-error_status">
+        <h1 class="title is-1 has-text-primary l-error_status--statusCode">
+          {{ error.statusCode }}
+        </h1>
+        <h2 class="subtitle is-4 has-text-primary l-error_status--message">
+          <template v-if="message[error.statusCode] === undefined">
+            suman
+          </template>
+          <template v-else>
+            {{ message[error.statusCode] }}
+          </template>
+        </h2>
+        <nuxt-link to="/" class="button is-rounded is-outlined is-primary is-medium">
+          TOP
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -36,6 +28,7 @@ import SiteFooter from '~/components/SiteFooter';
 export default {
   name: 'error',
   props: ['error'],
+  layout:'top',
   head() {
     return {
       title: this.error.statusCode,
