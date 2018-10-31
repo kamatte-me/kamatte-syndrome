192cba35eebf84cec988687d263b3ed53ac7d7a8
kamatte <uydasan@gmail.com>
2018-10-31 23:01:39 +0900

    mod: 画像と動画のビルドファイル名を変更 mod: 不要になったオプションを削除

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -78,14 +78,11 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    postcss: {
-      plugins: {
-        'postcss-custom-properties': {
-          warnings: false,
-        },
-      },
-    },
     extractCSS: true,
+    filenames: {
+      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name]_[hash:7].[ext]',
+      video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[name]_[hash:7].[ext]',
+    },
     /*
     ** Run ESLint on save
     */
