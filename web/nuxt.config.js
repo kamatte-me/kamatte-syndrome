7d4474e5036cbd80e0df1c79571f6e2743254b7d
kamatte <uydasan@gmail.com>
2019-06-03 04:43:35 +0900

    up: Issueが解決されたため対応コード削除

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -51,30 +51,6 @@ module.exports = {
     '@/assets/css/main.scss',
   ],
   /*
-  ** Router configuration
-  */
-  router: {
-    // 異なるLayout間を遷移した時にスクロール位置が保持されるIssueを解消
-    // https://github.com/nuxt/nuxt.js/issues/2738
-    // TODO: Issue Close後に削除
-    scrollBehavior(to, from, savedPosition) {
-      if (savedPosition) {
-        return savedPosition
-      } else {
-        let position = {};
-        if (to.matched.length < 2) {
-          position = { x: 0, y: 0 }
-        } else if (to.matched.some(r => r.components.default.options.scrollToTop)) {
-          position = { x: 0, y: 0 }
-        }
-        if (to.hash) {
-          position = { selector: to.hash }
-        }
-        return position
-      }
-    }
-  },
-  /*
   ** Build configuration
   */
   build: {
