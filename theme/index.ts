d3d4983a40d098535d72a51b17fc281d8ebf7cd9
kamatte <uydasan@gmail.com>
2021-05-07 14:00:37 +0900

    mod: 引用のデザイン

--- theme/index.ts
+++ theme/index.ts
@@ -114,11 +114,20 @@ export const theme: Theme = {
     },
     blockquote: {
       position: 'relative',
-      px: 4,
+      pl: 5,
+      pr: 4,
       py: 3,
       my: 2,
       fontStyle: 'italic',
       bg: 'lightgray',
+      '::before': {
+        display: 'inline-block',
+        position: 'absolute',
+        top: 2,
+        left: 3,
+        content: `url('data:image/svg+xml;utf-8,<svg stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.1" viewBox="0 0 16 18" height="36" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M3.516 7c1.933 0 3.5 1.567 3.5 3.5s-1.567 3.5-3.5 3.5-3.5-1.567-3.5-3.5l-0.016-0.5c0-3.866 3.134-7 7-7v2c-1.336 0-2.591 0.52-3.536 1.464-0.182 0.182-0.348 0.375-0.497 0.578 0.179-0.028 0.362-0.043 0.548-0.043zM12.516 7c1.933 0 3.5 1.567 3.5 3.5s-1.567 3.5-3.5 3.5-3.5-1.567-3.5-3.5l-0.016-0.5c0-3.866 3.134-7 7-7v2c-1.336 0-2.591 0.52-3.536 1.464-0.182 0.182-0.348 0.375-0.497 0.578 0.179-0.028 0.362-0.043 0.549-0.043z" /></svg>')`,
+        opacity: 0.2,
+      },
     },
     pre: {
       fontFamily: 'monospace',
