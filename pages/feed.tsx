9f3b3282b1cd3bf8a9c1296bb2fe263a292bb25c
kamatte <uydasan@gmail.com>
2021-05-10 02:07:32 +0900

    add: RSS自動生成

---
+++ pages/feed.tsx
@@ -0,0 +1,67 @@
+import { Author, Feed } from 'feed';
+import { writeFile } from 'fs/promises';
+import { GetStaticProps, NextPage } from 'next';
+import Head from 'next/head';
+
+import {
+  author as authorName,
+  baseUrl,
+  siteName,
+  slogan,
+} from '@/constants/site';
+import { client } from '@/lib/microcms';
+
+export const getStaticProps: GetStaticProps = async () => {
+  const author: Author = {
+    name: authorName,
+    link: `${baseUrl}/biography`,
+  };
+
+  const feed = new Feed({
+    title: siteName,
+    description: slogan,
+    id: `${baseUrl}`,
+    link: `${baseUrl}`,
+    language: 'ja',
+    image: `${baseUrl}/logo.png`,
+    favicon: `${baseUrl}/favicon.ico`,
+    copyright: `© ${new Date().getFullYear()} ${siteName}`,
+    author,
+  });
+
+  const blogEntries = await client.getAllContents('blog', {
+    fields: '',
+  });
+
+  blogEntries.forEach(entry => {
+    const url = `${baseUrl}/blog/${entry.id}`;
+    feed.addItem({
+      title: entry.title,
+      id: url,
+      link: url,
+      date: new Date(entry.publishedAt),
+      description: entry.body,
+      content: entry.body,
+      image: entry.featuredImage
+        ? entry.featuredImage.url
+        : `${baseUrl}/avatar.png`,
+      author: [author],
+    });
+  });
+
+  await writeFile(`${process.cwd()}/public/feed.xml`, feed.rss2());
+  return {
+    props: {},
+  };
+};
+
+const RSSFeed: NextPage = () => {
+  return (
+    <Head>
+      <meta httpEquiv="refresh" content="0; url=/feed.xml" />
+      <title>Redirect to /feed.xml</title>
+    </Head>
+  );
+};
+
+export default RSSFeed;
