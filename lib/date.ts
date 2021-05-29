a700d0f5613b987fe1ad82df494cab7c0d5d44aa
kamatte <uydasan@gmail.com>
2021-05-29 13:07:09 +0900

    SSGのHTMLの日付タイムゾーンがずれるの修正

--- lib/date.ts
+++ lib/date.ts
@@ -1,4 +1,7 @@
-export const formatDate = (date: string): string => {
-  const d = new Date(date);
+const offset = (-9 * 60 - new Date().getTimezoneOffset()) * 60 * 1000;
+
+export const formatDate = (dateStr: string): string => {
+  const d = new Date(dateStr);
+  d.setTime(d.getTime() - offset); // JSTにする
   return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`;
 };
