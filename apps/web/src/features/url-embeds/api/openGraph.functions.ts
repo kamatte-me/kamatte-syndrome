65cd65467a38327622687e62c467c5db1a6cd7d4
kamatte <uydasan@gmail.com>
2026-04-30 19:09:58 +0900

    refactor: statically import open graph server helper

--- apps/web/src/features/url-embeds/api/openGraph.functions.ts
+++ apps/web/src/features/url-embeds/api/openGraph.functions.ts
@@ -1,11 +1,9 @@
 import { createServerFn } from '@tanstack/react-start';
+import { fetchOpenGraphMetadata } from '../server/openGraph.server';
 import { validateOpenGraphRequest } from '../utils/openGraph';
 
 export const getOpenGraph = createServerFn({ method: 'GET' })
   .inputValidator(validateOpenGraphRequest)
   .handler(async ({ data }) => {
-    const { fetchOpenGraphMetadata } = await import(
-      '../server/openGraph.server'
-    );
     return fetchOpenGraphMetadata(data.url);
   });
