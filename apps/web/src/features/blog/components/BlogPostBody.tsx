20cf88efbfcd647abd0a4548b764bc53814116fb
kamatte <uydasan@gmail.com>
2026-07-08 01:07:10 +0900

    Adjust blog post code styling

--- apps/web/src/features/blog/components/BlogPostBody.tsx
+++ apps/web/src/features/blog/components/BlogPostBody.tsx
@@ -13,6 +13,8 @@ export function BlogPostBody({ className, ...props }: BlogPostBodyProps) {
       className={cn(
         'prose-img:mx-auto prose-img:block prose-img:h-auto prose-img:max-h-[360px] prose-img:w-auto prose-img:max-w-[min(100%,480px)] prose-img:object-contain',
         'prose-h2:border-cutout-hole prose-h2:border-b prose-h2:pb-3 prose-h1:text-2xl prose-h2:text-2xl prose-h3:text-xl prose-h4:text-lg prose-h5:text-base prose-headings:leading-tight sm:prose-h1:text-3xl sm:prose-h2:text-3xl sm:prose-h3:text-2xl sm:prose-h4:text-xl sm:prose-h5:text-lg',
+        'prose-pre:border-0',
+        '[&_:not(pre)>code]:mx-0.5 [&_:not(pre)>code]:rounded-sm [&_:not(pre)>code]:border [&_:not(pre)>code]:border-cutout-muted [&_:not(pre)>code]:bg-transparent [&_:not(pre)>code]:px-1 [&_:not(pre)>code]:py-0.5 [&_:not(pre)>code]:before:content-none [&_:not(pre)>code]:after:content-none',
         className,
       )}
     />
